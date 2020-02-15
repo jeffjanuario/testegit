62226ef7c1055d0c55d00b41b4eda6319b4280fe
jeffjanuario <61093448+jeffjanuario@users.noreply.github.com>
2020-02-15 16:41:30 -0300

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res)=>{
-    res.status(200).send({message: 'Hello World!'});
+    res.status(200).send({message: 'Hello!'});
 });
 
 app.listen(3001, ()=>{
